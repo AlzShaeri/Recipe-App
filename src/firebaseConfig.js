8fd99b38ec3f7774ed3072f0df9d1ef7af9ae0e8
Alireza Sh. <a3shaeri@uwaterloo.ca>
2024-09-05 14:48:43 -0400

    Resolved Secret Issues

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCl3uXr6wuKM59u59dNrGMg7C6sf4R1o5Y",
-  authDomain: "recipe-app-53bdd.firebaseapp.com",
-  projectId: "recipe-app-53bdd",
-  storageBucket: "recipe-app-53bdd.appspot.com",
-  messagingSenderId: "442989971442",
-  appId: "1:442989971442:web:9a8f8d0e5b20da9f4e8d6b",
-  measurementId: "G-0KQS33769L"
+  apiKey: "SAMPLE",
+  authDomain: "SAMPLE",
+  projectId: "SAMPLE",
+  storageBucket: "SAMPLE",
+  messagingSenderId: "SAMPLE",
+  appId: "SAMPLE",
+  measurementId: "SAMPLE"
 };
 
 const app = initializeApp(firebaseConfig);
